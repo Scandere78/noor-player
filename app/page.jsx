7a0041ca47444375688266a039e9c8579a43890d
ScandereT78 <scanderetejpro@gmail.com>
2025-02-12 00:09:23 +0100

    Start the project noor Coranique

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+      <div className="text-center py-10">
+        <h2 className="text-3xl font-bold text-green-700">Bienvenue sur Noor Player</h2>
+        <p className="text-gray-600 mt-2">Écoutez et lisez le Coran facilement.</p>
+      </div>
+    );
+  }
+  
